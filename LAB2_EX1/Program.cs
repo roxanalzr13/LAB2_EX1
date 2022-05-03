830c0371f39114bfeb272c65fec9c82eb6db474d
ROXANA lazarescu <roxana_lazarescu13@yahoo.com>
2022-05-03 22:48:17 +0300

    Add project files.

---
+++ LAB2_EX1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LAB2_EX1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Calculeaza n!
+            // n!=1*2*3*...*n
+
+            Console.WriteLine("Introduceti numarul n=");
+            int n = int.Parse(Console.ReadLine());
+            if (n == 0)
+            {
+                Console.WriteLine("Nu este corect");
+
+
+                return;
+            }
+            double factorial = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                factorial = factorial * i;
+            }
+
+             Console.WriteLine(factorial);
+     
+            
+
+
+
+        }   
+    }
+}
